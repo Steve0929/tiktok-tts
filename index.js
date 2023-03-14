fa95ebebc5316018860767ee61cb945fc1236667
Steve0929 <chimi2606@hotmail.com>
2023-03-13 20:27:03 -0500

    basic functionality working

--- index.js
+++ index.js
@@ -1,25 +1,59 @@
-const axios = require("axios");
+const fs = require('fs');
+const axios = require('axios');
 const BASE_URL = 'https://api16-normal-c-useast1a.tiktokv.com/media/api/text/speech/invoke';
+const DEFAULT_VOICE = 'en_us_001';
 
+let tiktokSessionId = null;
 
-async function createAudioFromText(text, path) {
+function config(userTiktokSessionId) {
+    tiktokSessionId = userTiktokSessionId
+}
+
+function prepareText(text) {
+    text = text.replace('+', 'plus');
+    text = text.replace(/\s/g, '+');
+    text = text.replace('&', 'and');
+    return text;
+}
+
+function handleStatusError(status_code) {
+    switch (status_code) {
+        case 1:
+            throw new Error(`Your TikTok session id might be invalid or expired. Try getting a new one. status_code: ${status_code}`);
+        case 2:
+            throw new Error(`The provided text is too long. status_code: ${status_code}`);
+        case 4:
+            throw new Error(`Invalid speaker, please check the list of valid speaker values. status_code: ${status_code}`);
+        case 5:
+            throw new Error(`No session id found. status_code: ${status_code}`);
+    }
+}
+
+async function createAudioFromText(text, fileName, speaker) {
+    const text_speaker = speaker ? speaker : DEFAULT_VOICE;
     const req_text = prepareText(text);
-    const URL = `${BASE_URL}/?text_speaker=${speaker}&req_text=${req_text}&speaker_map_type=0&aid=1233`;
+    const URL = `${BASE_URL}/?text_speaker=${text_speaker}&req_text=${req_text}&speaker_map_type=0&aid=1233`;
     const headers = {
         'User-Agent': 'com.zhiliaoapp.musically/2022600030 (Linux; U; Android 7.1.2; es_ES; SM-G988N; Build/NRD90M;tt-ok/3.12.13.1)',
         'Cookie': `sessionid=${tiktokSessionId}`,
         'Accept-Encoding': 'gzip,deflate,compress'
     }
-    const result = await axios.post(URL, null, { headers: headers });
-    const encoded_voice = result?.data?.data?.v_str;
-    fs.writeFileSync(`${fileName}`, Buffer.from(encoded_voice, 'base64'));
+    try {
+        const result = await axios.post(URL, null, { headers: headers });
+        const status_code = result?.data?.status_code;
+        if (status_code !== 0) return handleStatusError(status_code);
+        const encoded_voice = result?.data?.data?.v_str;
+        fs.writeFileSync(`${fileName}.mp3`, Buffer.from(encoded_voice, 'base64'));
+    } catch (err) {
+        throw new Error(`tiktok-tts ${err}`);
+    }
 }
 
-function prepareText(text) {
-    text = text.replace("+", "plus");
-    text = text.replace(/\s/g, "+");
-    text = text.replace("&", "and");
-    return text;
-}
 
-module.exports = createAudioFromText
+module.exports = {
+    config: function (tiktokSessionId) { return config(tiktokSessionId) },
+    createAudioFromText: async function (text, path, speaker) {
+        if (tiktokSessionId) return createAudioFromText(text, path, speaker)
+        throw new Error(`tiktok-tts has not been configured! Make sure you run config(yourTiktokSessionIdHere)`);
+    }
+}
---
+++ voices.js
@@ -0,0 +1,71 @@
+const voices = {
+    "english": [
+        "en_au_001",  // English AU - Female
+        "en_au_002",  // English AU - Male
+        "en_uk_001",  // English UK - Male 1
+        "en_uk_003",  // English UK - Male 2
+        "en_us_001",  // English US - Female (Int. 1)
+        "en_us_002",  // English US - Female (Int. 2)
+        "en_us_006",  // English US - Male 1
+        "en_us_007",  // English US - Male 2
+        "en_us_009",  // English US - Male 3
+        "en_us_010",  // English US - Male 4
+        "en_male_narration",  // Narrator
+        "en_male_funny",  // Funny
+        "en_female_emotional",  // Peaceful
+        "en_male_cody",  // Serious
+    ],
+    "disney": [
+        "en_us_ghostface",  // Ghost Face
+        "en_us_chewbacca",  // Chewbacca
+        "en_us_c3po",       // C3PO
+        "en_us_stitch",     // Stitch
+        "en_us_stormtrooper",  // Stormtrooper
+        "en_us_rocket",  // Rocket
+        "en_female_madam_leota",  // Madame Leota
+        "en_male_ghosthost",  // Ghost Host
+        "en_male_pirate",  // pirate
+    ],
+    "french": [
+        "fr_001",  // French - Male 1
+        "fr_002",  // French - Male 2
+    ],
+    "spanish": [
+        "es_002",     // Spanish (spain) - Male
+        "es_mx_002",  // Spanish MX - Male
+    ],
+    "portuguese": [
+        "br_001",  // Portuguese BR - Female 1
+        "br_003",  // Portuguese BR - Female 2
+        "br_004",  // Portuguese BR - Female 3
+        "br_005",  // Portuguese BR - Male
+    ],
+    "german": [
+        "de_001",  // German - Female
+        "de_002",  // German - Male
+    ],
+    "indonesian":[
+        "id_001",  // Indonesian - Female
+    ],
+    "japanese": [
+        "jp_001",  // Japanese - Female 1
+        "jp_003",  // Japanese - Female 2
+        "jp_005",  // Japanese - Female 3
+        "jp_006",  // Japanese - Male
+    ],
+    "korean": [
+        "kr_002",  // Korean - Male 1
+        "kr_003",  // Korean - Female
+        "kr_004",  // Korean - Male 2
+    ],
+    "other": [
+        "en_female_f08_salut_damour",  // Alto
+        "en_male_m03_lobby",  // Tenor
+        "en_male_m03_sunshine_soon",  // Sunshine Soon
+        "en_female_f08_warmy_breeze",  // Warmy Breeze
+        "en_female_ht_f08_glorious",  // Glorious
+        "en_male_sing_funny_it_goes_up",  // It Goes Up
+        "en_male_m2_xhxs_m03_silly",  // Chipmunk
+        "en_female_ht_f08_wonderful_world",  // Dramatic
+    ]
+}
